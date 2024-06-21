4283177ebf2258cd424c288220967ec065be51fd
xiaoyu <jiangzhong91@gmail.com>
2024-06-21 14:39:26 +0800

    init mock

---
+++ manager/mock/index.js
@@ -0,0 +1,5 @@
+const user = require('./user')
+
+export default {
+    ...user,
+}
---
+++ manager/mock/user.js
@@ -0,0 +1,49 @@
+const data = []
+
+function users() {
+    for (let i = 0; i < 9; i++) {
+        data.push({
+            uid: 'user' + i,
+            userName: 'userName' + i,
+            nickName: 'nickName' + i,
+            email: 'email' + i + '@gmail.com',
+            phone: '1889999000' + i,
+            qq: '998877660' + i,
+        })
+    }
+    return data;
+}
+
+const mockData = {
+    'GET /v1/api/user': users(),
+    'POST /v1/api/user': (req, res) => {
+        const {uid, userName, nickName, email, phone, qq} = req.body;
+        data.push({
+            uid: uid, userName: userName, nickName: nickName, email: email, phone: phone, qq: qq
+        })
+        return res.json({
+            status: 'ok'
+        });
+    },
+    'PUT /v1/api/user': (req, res) => {
+        const {uid, userName, nickName, email, phone, qq} = req.body;
+        const getData = {uid: uid, userName: userName, nickName: nickName, email: email, phone: phone, qq: qq}
+        for (let i = 0; i < data.length; i++) {
+            if (uid === data[i].uid) {
+                data[i] = getData
+            }
+        }
+        res.send({status: 'ok', message: 'update success！'});
+    },
+    'DELETE /v1/api/user/:uid': (req, res) => {
+        const uid = req.params.uid
+        for (let i = 0; i < data.length; i++) {
+            if (uid === data[i].uid) {
+                data.splice(i, i + 1)
+            }
+        }
+        res.send({status: 'ok', message: 'delete success!'})
+    },
+}
+
+module.exports = mockData
