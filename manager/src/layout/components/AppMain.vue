60e81955a1691e5512a279527847fc6ac1b3d803
xiaoyu <jiangzhong91@gmail.com>
2024-06-22 01:05:11 +0800

    update router not cache

--- manager/src/layout/components/AppMain.vue
+++ manager/src/layout/components/AppMain.vue
@@ -11,11 +11,12 @@ export default {
 
 <template>
   <el-main>
-    <transition name="fade-transform" mode="out-in">
-<!--      <keep-alive>-->
-        <router-view :key="key"/>
-<!--      </keep-alive>-->
-    </transition>
+    <!--  //TODO 子路由修改为缓存，暂时不缓存  -->
+    <!--    <transition name="fade-transform" mode="out-in">-->
+    <!--      <keep-alive>-->
+    <router-view :key="key"/>
+    <!--      </keep-alive>-->
+    <!--    </transition>-->
   </el-main>
 </template>
 
