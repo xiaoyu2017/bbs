f067cee40b75b6bdb0e8e54bc94a26ead0cf7b11
xiaoyu <jiangzhong91@gmail.com>
2024-06-21 14:40:36 +0800

    init router

--- manager/src/router/index.js
+++ manager/src/router/index.js
@@ -4,20 +4,138 @@ import Layout from '@/layout'
 
 Vue.use(VueRouter)
 
-const routes = [
+export const routes = [
     {
         path: '/',
         name: 'home',
         component: Layout,
-        redirect: '/index',
+        redirect: '/dashboard',
+        meta: {
+            title: 'Dashboard',
+            icon: 'el-icon-s-data'
+        },
+        children: [
+            {
+                path: 'dashboard',
+                component: () => import('@/views/dashboard/index'),
+                name: 'Dashboard',
+                meta: {title: 'dashboard'}
+            }
+        ]
+    },
+    {
+        path: '/user',
+        name: 'User',
+        component: Layout,
+        meta: {
+            title: 'User',
+            icon: 'el-icon-user-solid'
+        },
+        hasChildren: true,
+        children: [
+            {
+                path: 'controller',
+                component: () => import('@/views/user/controller'),
+                name: 'controller',
+                meta: {title: 'controller', icon: 'el-icon-minus'}
+            },
+            {
+                path: 'manager',
+                component: () => import('@/views/user/manager'),
+                name: 'manager',
+                meta: {title: 'manager', icon: 'el-icon-minus'}
+            }
+        ]
+    },
+    {
+        path: '/menu',
+        name: 'Menu',
+        component: Layout,
+        redirect: '/menu/index',
+        meta: {
+            title: 'Menu',
+            icon: 'el-icon-menu'
+        },
         children: [
             {
                 path: 'index',
-                component: () => import('@/views/user/index'),
-                name: 'User',
-                meta: {title: 'User'}
+                component: () => import('@/views/menu/index'),
+                name: 'index',
             }
         ]
+    },
+    {
+        path: '/role',
+        name: 'Role',
+        component: Layout,
+        redirect: '/role/index',
+        meta: {
+            title: 'Role',
+            icon: 'el-icon-files'
+        },
+        children: [
+            {
+                path: 'index',
+                component: () => import('@/views/role/index'),
+                name: 'index',
+            }
+        ]
+    },
+    {
+        path: '/group',
+        name: 'Group',
+        component: Layout,
+        redirect: '/group/index',
+        meta: {
+            title: 'Group',
+            icon: 'el-icon-s-custom'
+        },
+        children: [
+            {
+                path: 'index',
+                component: () => import('@/views/group/index'),
+                name: 'index',
+            }
+        ]
+    },
+    {
+        path: '/attachment',
+        name: 'Attachment',
+        component: Layout,
+        redirect: '/attachment/index',
+        meta: {
+            title: 'Attachment',
+            icon: 'el-icon-picture'
+        },
+        children: [
+            {
+                path: 'index',
+                component: () => import('@/views/attachment/index'),
+                name: 'index',
+            }
+        ]
+    },
+    {
+        path: '/setting',
+        name: 'Setting',
+        component: Layout,
+        redirect: '/setting/index',
+        meta: {
+            title: 'Setting',
+            icon: 'el-icon-setting'
+        },
+        children: [
+            {
+                path: 'index',
+                component: () => import('@/views/setting/index'),
+                name: 'index',
+            }
+        ]
+    },
+    {
+        path: '/login',
+        component: () => import('@/views/login/index'),
+        hidden: true
     }
 ]
 
