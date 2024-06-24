1af4c72948a5c82b2f01cc9a661ea2c51d8e7eb2
xiaoyu <jiangzhong91@gmail.com>
2024-06-24 23:26:23 +0800

    update user api

--- manager/src/api/user.js
+++ manager/src/api/user.js
@@ -1,13 +1,21 @@
 import request from '@/utils/request'
 
 export function getUsersByPage(page, num) {
-    let config ={
+    let config = {
         url: '/v1/api/user',
         method: 'get',
         params: {
             page: page,
             num: num
         }
+    }
+    return request(config)
+}
+
+export function deleteUsersByUid(uid) {
+    let config = {
+        url: '/v1/api/user/' + uid,
+        method: 'delete',
     }
     return request(config)
 }
