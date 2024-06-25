8cdb96e635396747a0ef3b479726448deef62cae
xiaoyu <jiangzhong91@gmail.com>
2024-06-26 02:04:06 +0800

    add menu api

--- manager/src/api/menu.js
+++ manager/src/api/menu.js
@@ -2,7 +2,22 @@ import request from '@/utils/request'
 
 export function getMenu() {
     return request({
-        url: '/menu',
+        url: '/v1/api/menu',
         method: 'get'
     })
+}
+
+export function getMenuChildren(id) {
+    return request({
+        url: '/v1/api/menu/' + id,
+        method: 'get'
+    })
+}
+
+export function deleteMenuByUid(id) {
+    let config = {
+        url: '/v1/api/menu/' + id,
+        method: 'delete',
+    }
+    return request(config)
 }
