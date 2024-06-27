f00ba29060736a55dad22dd223afed5c37ed2ebb
xiaoyu <jiangzhong91@gmail.com>
2024-06-28 01:59:03 +0800

    update article view

--- manager/src/views/article/ArticleEditor.vue
+++ manager/src/views/article/ArticleEditor.vue
@@ -1,73 +1,45 @@
 <template>
   <el-row class="box">
     <el-col class="header-box">
-      <el-input style="float: left;" size="small" type="text" placeholder="请输入文章标题">
-        <el-button slot="append" >提交</el-button>
+      <el-input style="float: left;" size="small" type="text" placeholder="请输入文章标题" v-model="title">
+        <el-button slot="append" @click="submitArticle">提交</el-button>
       </el-input>
     </el-col>
     <el-col class="editor-box">
-      <div id="editor" ref="editor"></div>
+      <markdown-editor ref="editor"/>
     </el-col>
   </el-row>
-
 </template>
 
 <script>
-import '@toast-ui/editor/dist/toastui-editor.css';
-// 工具栏显示中文
-// import '@toast-ui/editor/dist/i18n/zh-cn.js';
-import '@toast-ui/editor/dist/toastui-editor-viewer.css';
-// Toast UI Editor 配置文件
-import codeSyntaxHighlight from '@toast-ui/editor-plugin-code-syntax-highlight';
-import Editor from '@toast-ui/editor';
+import markdownEditor from "@/components/MarkdownEditor/index.vue";
+import request from "@/utils/request";
 
 export default {
-  name: 'markingRecord',
+  name: 'ArticleEditor',
+  components: {markdownEditor},
   data() {
     return {
-      editor: null,
-      markdown: `# Hello, World! :icon: ### 被告人谭某某，女，1974
-          ##### 指定辩护人胡建和，广东瑞霆律师事务所律师。
-        `,
-    };
+      title: ''
+    }
   },
   mounted() {
-    this.initEditor();
-    // 必须放mounted里面，放created里面会导致初始化没成功，编辑器不能正常使用
+    console.log(JSON.stringify(this.$route.params.article))
   },
   methods: {
-    initEditor() {
-      this.editor = new Editor({
-        el: document.querySelector('#editor'),
-        // 绑定到id为editor的节点
-        previewStyle: 'vertical',
-        // 编辑器的预览样式
-        initialValue: this.markdown,
-        // 编辑器的初始内容
-        initialEditType: 'markdown',
-        // 编辑器的模式，（编辑模式markdown/所见即所得模式wysiwyg）
-        hideModeSwitch: true,
-        // 是否隐藏上面的编辑器模式选项卡
-        useCommandShortcut: true,
-        // 是否使用键盘快捷键执行命令（默认是true）
-        height: '100%',
-        // 编辑器高度
-        previewHighlight: false,
-        // 是否高亮预览区域 (默认false)
-        language: 'zh-CN',
-        plugins: [codeSyntaxHighlight]
-
-      });
-      this.editor.on('change', () => {
-        //这里是editor内容发生改变执行的方法
-        //如果要监听内容变化并依据条件执行可以在这里写入触发方法
-      });
-      this.editor.on('load', () => {
-        //设置editor的文本内容靠左（我用的时候是默认居中的，但我需要内容默认靠左所以需要加上这个）
-        this.editor.exec('textAlign', 'left');
-      });
+    submitArticle() {
+      let articleData = {}
+      articleData.html = this.$refs.editor.getHtml()
+      articleData.value = this.$refs.editor.getValue()
+      articleData.title = this.title
+      request({
+        url: '/v1/api/article',
+        method: 'post',
+        data: articleData
+      }).then(response => {
+        console.log(JSON.stringify(response))
+      })
     },
-
   },
 };
 </script>
--- manager/src/views/article/ArticleManager.vue
+++ manager/src/views/article/ArticleManager.vue
@@ -1,44 +1,167 @@
-<template>
-  <div>
-    <MarkdownEditor ref="markdownEditor" v-model="content1" height="500px" />
-  </div>
-</template>
-
 <script>
-import MarkdownEditor from '@/components/MarkdownEditor';
+import {Message, MessageBox} from "element-ui"
+import {deleteArticleByUid, getArticlesByPage} from "@/api/article";
 
-const content = `
-**This is test**
-
-* vue
-* element
-* webpack
-`
 export default {
   name: "ArticleManager",
-  components: {
-    MarkdownEditor,
-  },
   data() {
     return {
-      html: '',
-      value: '',
-      content1: content,
+      tableData: [],
+      search: '',
+      currentPage: 1,
+      pageSize: 10,
+      total: 0
     }
   },
   methods: {
-    getHtml() {
-      this.html = this.$refs.markdownEditor.getHtml();
-      console.log(this.html);
+    handleEdit(index, row) {
+      this.$router.push({name: 'ArticleEditor', params: {article: row}})
     },
-    getValue() {
-      this.value = this.$refs.markdownEditor.getValue();
-      console.log(this.value);
+    handleDelete(index, row) {
+      MessageBox.confirm('此操作将永久删除该文章, 是否继续?', '提示', {
+        confirmButtonText: '确定',
+        cancelButtonText: '取消',
+        type: 'warning'
+      }).then(() => {
+        deleteArticleByUid(row.id).then(() => {
+          Message({
+            type: 'success',
+            message: '删除成功!'
+          });
+          this.updateTableData(this.currentPage, this.pageSize)
+        }).catch(() => {
+          Message({
+            type: 'error',
+            message: '删除失败!'
+          })
+        })
+      }).catch(() => {
+        Message({
+          type: 'info',
+          message: '已取消删除'
+        });
+      });
     },
-  }
+    handleSizeChange(val) {
+      this.pageSize = val
+      this.updateTableData(this.currentPage, this.pageSize)
+    },
+    handleCurrentChange(val) {
+      this.currentPage = val
+      this.updateTableData(this.currentPage, this.pageSize)
+    },
+    addArticle() {
+      this.$router.push('/article/ArticleEditor')
+    },
+    updateTableData(page, num) {
+      // 获得用户信息
+      getArticlesByPage(page, num).then(response => {
+        this.tableData = response.data.data
+        this.total = response.data.total
+        this.currentPage = page
+      })
+    },
+  },
+  created() {
+    this.updateTableData(1, 10)
+  },
+
 }
 </script>
 
-<style lang="scss" scoped>
+<template>
+  <el-row class="box">
+    <el-col class="table-box">
+      <el-table
+          :data="tableData"
+          border
+          stripe
+          height="100%"
+          max-height="100%">
+        <el-table-column>
+          <template slot="header" slot-scope="scope">
+            <el-row>
+              <el-col :span="2">
+                <el-button style="float: left" type="primary" @click="addArticle">新增文章</el-button>
+              </el-col>
+              <el-col :span="16" style="padding: 0 10px">
+                <el-alert
+                    title="表格当前显示数据条件"
+                    type="warning"
+                    show-icon
+                    :closable="false">
+                </el-alert>
+              </el-col>
+              <el-col :span="6">
+                <el-input style="float: right;height: 40px;" type="text" placeholder="输入关键字搜索" v-model="search">
+                  <el-button slot="append" icon="el-icon-search"></el-button>
+                </el-input>
+              </el-col>
+            </el-row>
+          </template>
 
+          <el-table-column prop="id" label="ID" width="150"></el-table-column>
+          <el-table-column prop="title" label="标题" width="200" :show-overflow-tooltip="true"></el-table-column>
+          <el-table-column prop="value" label="内容值" :show-overflow-tooltip="true"></el-table-column>
+          <el-table-column prop="html" label="内容html" width="200" :show-overflow-tooltip="true"></el-table-column>
+
+          <el-table-column label="操作" width="120">
+            <template slot-scope="scope">
+              <el-button type="primary"
+                         icon="el-icon-edit"
+                         @click="handleEdit(scope.$index, scope.row)"
+                         circle>
+              </el-button>
+              <el-button
+                  type="danger"
+                  icon="el-icon-delete"
+                  circle
+                  @click="handleDelete(scope.$index, scope.row)">
+              </el-button>
+            </template>
+          </el-table-column>
+        </el-table-column>
+      </el-table>
+    </el-col>
+    <el-col class="table-page-box">
+      <el-pagination
+          @size-change="handleSizeChange"
+          @current-change="handleCurrentChange"
+          :current-page="currentPage"
+          :page-sizes="[10, 15, 20, 30]"
+          :page-size="pageSize"
+          layout="total, sizes, prev, pager, next, jumper"
+          :total="total">
+      </el-pagination>
+    </el-col>
+    <common-dialog/>
+  </el-row>
+</template>
+
+<style scoped>
+.box {
+  height: 100%;
+  position: relative;
+}
+
+.table-box {
+  top: 0;
+  bottom: 35px;
+  position: absolute;
+}
+
+el-table {
+  width: 100%;
+}
+
+.table-page-box {
+  height: 35px;
+  width: 100%;
+  bottom: 0;
+  position: absolute;
+  border: 1px solid #EBEEF5;
+  background-color: #FFF;
+  text-align: center;
+  line-height: 35px;
+}
 </style>
