496c18c6321a2cb7f5c3d0c5dbf62cc7c186c3f5
xiaoyu <jiangzhong91@gmail.com>
2024-06-29 23:58:26 +0800

    init layout component

---
+++ web/src/layout/LayoutBar.vue
@@ -0,0 +1,56 @@
+<script>
+export default {
+  name: "LayoutBar",
+  data() {
+    return{
+      links: [
+        '主页',
+        '新帖',
+        '搜索',
+        '开发',
+        '娱乐',
+        '软件',
+        '资源',
+        '关于',
+      ],
+    }
+  },
+}
+</script>
+
+<template>
+  <v-app-bar app color="grey darken-1" flat>
+    <v-container class="py-0 fill-height">
+      <!-- 徽章 -->
+      <v-badge bordered bottom color="deep-purple accent-4" dot offset-x="10" offset-y="10" style="margin-right: 10px;">
+        <v-avatar size="40" >
+          <v-img src="https://cdn.vuetifyjs.com/images/lists/2.jpg"></v-img>
+        </v-avatar>
+      </v-badge>
+
+      <v-btn class="white--text" v-for="link in links" :key="link" text >  {{ link }} </v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-responsive max-width="260">
+        <v-row>
+          <v-col>
+            <v-text-field
+                dense
+                flat
+                hide-details
+                rounded
+                solo-inverted
+                label="输入搜索内容"
+                prepend-inner-icon="mdi-magnify"
+            ></v-text-field>
+          </v-col>
+        </v-row>
+      </v-responsive>
+    </v-container>
+  </v-app-bar>
+</template>
+
+<style scoped>
+
+</style>
---
+++ web/src/layout/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app>
+
+    <layout-bar/>
+
+    <v-main class="grey lighten-3">
+      <v-container>
+        <router-view/>
+      </v-container>
+    </v-main>
+
+    <layout-footer/>
+
+  </v-app>
+</template>
+
+<script>
+import LayoutBar from "@/layout/LayoutBar.vue";
+import LayoutFooter from "@/layout/LayoutFooter.vue";
+
+export default {
+  components: {LayoutFooter, LayoutBar},
+  data: () => ({
+
+  }),
+}
+</script>
